517be020079c40a06815aba959464819baf39b73
gitMukhtar <reach2mukhtar.com>
2020-03-04 23:41:06 +0530

    Launch Confif

---
+++ vpc.tf
@@ -0,0 +1,139 @@
+# Create a VPC for cloudSpace
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  instance_tenancy     = "default"
+  enable_dns_support   = "true"
+  enable_dns_hostnames = "true"
+  enable_classiclink   = "false"
+  tags = {
+    Name = "main"
+  }
+}
+
+# Create a Public Subnet
+resource "aws_subnet" "main-public-1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-2a"
+
+  tags = {
+    Name = "main-public-1"
+  }
+}
+
+# Create a Public Subnet
+resource "aws_subnet" "main-public-2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone       = "us-east-2b"
+
+  tags = {
+    Name = "main-public-2"
+  }
+}
+
+
+# Create an Internet GW
+resource "aws_internet_gateway" "main-gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main"
+  }
+}
+
+# Create route tables
+resource "aws_route_table" "main-public-1" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-gw.id
+  }
+
+  tags = {
+    Name = "main-public-1"
+  }
+}
+
+# Create route associations public
+resource "aws_route_table_association" "main-public-1" {
+  subnet_id      = aws_subnet.main-public-1.id
+  route_table_id = aws_route_table.main-public-1.id
+}
+
+# Create a Security Group
+#resource "aws_security_group" "allow-ssh" {
+#  vpc_id      = aws_vpc.main.id
+#  name        = "allow-ssh"
+# description = "security group that allows ssh and all egress traffic"
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#   cidr_blocks = ["0.0.0.0/0"]
+#}
+
+# ingress {
+#   from_port   = 22
+#   to_port     = 22
+#  protocol    = "tcp"
+#   cidr_blocks = ["0.0.0.0/0"]
+#}
+#  tags = {
+#   Name = "allow-ssh"
+# }
+#}
+
+resource "aws_security_group" "myinstance" {
+  vpc_id      = aws_vpc.main.id
+  name        = "myinstance"
+  description = "security group for my instance"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.elb-securitygroup.id]
+  }
+
+  tags = {
+    Name = "myinstance"
+  }
+}
+
+resource "aws_security_group" "elb-securitygroup" {
+  vpc_id      = aws_vpc.main.id
+  name        = "elb"
+  description = "security group for load balancer"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "elb"
+  }
+}
